593dd36c144c231d479e556282b27d4c3955f255
JonathanCuestaSanchez <jonathancuesta97@gmail.com>
2019-09-05 18:15:23 -0500

    QRscanner

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 import QrReader from 'react-qr-reader'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: 'No result'
-    }
+class Scanner extends Component {
+  state = {
+    result: 'No result'
   }
+
   handleScan = data => {
     if (data) {
       this.setState({
@@ -21,15 +17,8 @@ class App extends React.Component {
     console.error(err)
   }
   render() {
-    const divStyle = {
-      heigth: '50vh',
-      width: '50vw',
-      display:'flex',
-     
-
-    };
     return (
-      <div style={divStyle}>
+      <div>
         <QrReader
           delay={300}
           onError={this.handleError}
@@ -41,5 +30,4 @@ class App extends React.Component {
     )
   }
 }
-
-export default App;
+export default Scanner;
