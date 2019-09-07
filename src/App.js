f05510ff3855d5b2486f756639823770724a59bc
alejandrordto <masterale2011@gmail.com>
2019-09-07 08:17:38 -0500

    login, services, y reserved

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import Services from './components/Services';
 import QrGenerator from './components/QrGenerator';
 import BikeReserve from './components/BikeReserve';
 import Confirmed from './components/Confirmed';
+import Scanner from "./Scanner"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { LoginHome } from "./components/LoginHome"
+import { Name } from "./components/Create_acount/Name"
+import { Information } from "./components/Create_acount/Information"
+import { Terms } from "./components/Create_acount/Terms"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  handleEvent = () => {
-    
-  }
   render() {
     return (
-      <div className="App">
-        
-        < Confirmed />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={LoginHome} />    
+          <Route path="/name" exact component={Name} />      
+          < Route path="/information" exact component={Information} />   
+          <Route path="/terms" exact component={Terms} />   
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Scanner.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+//import QrReader from 'react-qr-reader'
+
+class Scanner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: 'No result'
+        }
+    }
+    /*<QrReader
+    delay={300}
+    onError={this.handleError}
+    onScan={this.handleScan}
+    style={{ width: '100%', heigth: '100%' }}
+    showViewFinder={true}
+/>*/
+
+    handleScan = data => {
+        if (data) {
+            this.setState({
+                result: data
+            })
+        }
+    }
+    handleError = err => {
+        console.error(err)
+    }
+    render() {
+        const divStyle = {
+            heigth: '100vh',
+            width: '100vw',
+        };
+        return (
+            <div style={divStyle} >
+                <p>{this.state.result}</p>
+            </div>
+        )
+    }
+}
+export default Scanner;
