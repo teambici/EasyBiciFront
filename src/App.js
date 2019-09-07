48e37a4f271e6724646176aa8f41c3e69c134b93
Alejandrito <alejope98@hotmail.com>
2019-09-06 19:04:11 -0500

    ciclaMejorada

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login"
-import Scanner from "./Scanner"
-import QrGenerator from './QrGenerator';
-import Bike from "./components/Bike"
+import Login from "./components/Login";
+import { Bike } from "./components/Bike";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BikeReserve } from "./components/BikeReserve";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-  }  
-  handleEvent = () => {
-    
   }
+
   render() {
     return (
-      <div className="App">        
-        <Bike/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={Bike} />
+          <Route path="/bikereserve" exact component={BikeReserve} />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import bikerent from '../img/bikerent.jpg';
-class Bike extends Component {
+import {Redirect} from "react-router-dom";
 
-
-    render() {
-        return (
-            <div className="Bike">
-
-                <div>
-                    <img width='30%' src={bikerent} alt="bike" />
-                    <h2> Bicicleta de Carreras </h2>
-
-                </div>
-
-            </div>
-
-        );
+export class Bike extends Component {
+    constructor(props) {
+        super(props);
+        this.state={Check:false}
+        this.handleCheck = this.handleCheck.bind(this);
     }
 
+    handleCheck(event) {        
+        this.setState({Check: true});
+    } 
+    render() {        
+        if (this.state.Check) {
+            return <Redirect to={{
+                pathname: '/bikereserve'               
+            }}
+            />
+        }
+        const divStyle = {
+            display: 'flex',
+            flexDirection:'column',
+            justifyContent: 'space-between'
+        };        
+        return (
+            <div>
+                <img width='30%' src={bikerent} alt="bike" />
+                <h1> Bicicleta X </h1> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras laoreet lorem nulla, at molestie neque vestibulum nec. Nullam blandit nisi ligula, et fringilla orci euismod ullamcorper. Quisque a eros erat. Aenean elementum blandit tortor, eget semper lectus tempor id. Quisque eu ante posuere, euismod neque sed, porta dolor. Integer maximus eros a commodo ultrices. Phasellus molestie consectetur ex, at blandit tortor. Curabitur posuere, odio pellentesque molestie efficitur, turpis mi aliquam velit, eu condimentum leo nunc sed risus. Vivamus ligula est, placerat in imperdiet id, ornare vel mi. Praesent lectus enim, maximus a magna et, commodo efficitur orci. Cras hendrerit risus a elit accumsan congue. </p>               
+                <div style={divStyle}>
+                    <button onClick={this.handleCheck}> Check </button>                                       
+                </div>                
+            </div>
+        )
+    }
 }
 
 export default Bike;
