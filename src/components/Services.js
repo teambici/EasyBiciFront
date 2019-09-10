5a51880ab44ccc9d502f301a84a78f59c994d721
david <david.ramirez-mo@mail.escuelaing.edu.co>
2019-09-10 18:56:59 -0500

    localizar texto

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,22 +7,28 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 import Card from '@material-ui/core/Card';
-
+import { GoogleComponent } from 'react-google-location'
 import "./Services.css"
 import MenuUserLogged from "./MenuUserLogged.js"
 import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import Map from './Map';
 class Services extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { place: null };
+    }
+
 
 
     render() {
+        const API_KEY = "";
         return (
             <div>
                 <MenuUserLogged></MenuUserLogged>
 
                 <h2>Bienvenido {localStorage.getItem("mailLogged")}</h2>
-                <h3>Pedir ciclas</h3>
+                <h3>Buscar ciclas</h3>
                 <div>
                     <Card className="insertCard">
                         <form onSubmit={this.handleSubmit}>
@@ -33,25 +39,31 @@ class Services extends Component {
                                 onChange={this.handleChange}
                                 margin="normal"
                             />
+                            <GoogleComponent 
+                                apiKey={API_KEY}
+                                languaje={"en"}
+                                
+                            ></GoogleComponent>
+
                             <p></p>
 
-                           
+
                             <p></p>
                             <Button variant="contained" color="primary" type="submit">
-                                Agregar 
+                                Agregar
                             </Button>
                         </form>
                     </Card>
                     <br className="fix" />
-                    
+
                     <Map className="Map"></Map>
-                   
+
                 </div>
 
 
 
 
-            
+
             </div >
         );
     }
