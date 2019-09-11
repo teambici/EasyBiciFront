75673b9f31c42a7c6c9cd550bd49dba91705db87
alejandrordto <masterale2011@gmail.com>
2019-09-10 22:25:41 -0500

    confirmed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 
-import {Login} from './components/Login';
-import Services from './components/Services';
+import {Login} from './Components/Login';
+import Services from './Components/Services';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { LoginHome } from "./Components/LoginHome"
 import { Name } from "./Components/Create_acount/Name"
 import { Information } from "./Components/Create_acount/Information"
 import { Terms } from "./Components/Create_acount/Terms"
+import Confirmed from './Components/Confirmed';
+import qrGenerator from "./Components/QrGenerator";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,11 +21,13 @@ class App extends React.Component {
       <Router>
         <div className="App color_fondo">
           <Route path="/" exact component={LoginHome} />
+          <Route path="/qrGenerator" exact component={qrGenerator} />
           <Route path="/login" exact component={Login} />
           <Route path="/Services" exact component={Services} />     
           <Route path="/name" exact component={Name} />      
           <Route path="/information" exact component={Information} />   
           <Route path="/terms" exact component={Terms} />
+          <Route path="/Confirmed" exact component={Confirmed}/>
         </div>
       </Router>
     );
--- src/components/Confirmed.js
+++ src/components/Confirmed.js
@@ -1,67 +1,48 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import "./Confirmed.css";
+import Divider from '@material-ui/core/Divider';
+import Face from '@material-ui/icons/Face';
+import { Redirect } from "react-router-dom";
 class Confirmed extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = { qrButton: false};
+        this.handleqrButton = this.handleqrButton.bind(this);
+    }
+    handleqrButton(event){
+        this.setState({qrButton:true})
+    }
 
     render() {
-        const divStyle = {
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-
+        if(this.state.qrButton){
+            return <Redirect to={{
+                pathname: '/qrGenerator'
+            }}
+            />
         }
-        const leftStyle = {
-            marginRight: '1000px',
-            align: 'left',
-        }
-
         return (
+
             <div className=" Confirmed">
-
-                <div >
-
-                    <h2 style={leftStyle}>YOUR BIKE  </h2>
+                <h1 className="titulo">YOUR BIKE</h1>
+                <div />
+                <Divider />
+                <div>
+                    <Face className="usuario"></Face>
+                    <h6 className="usuario">usuario parker</h6>
                 </div>
-                <div style={divStyle}>
-                    <div>   </div>
-                    <div >
-                        <img src='../img/found' />
-                    </div>
-                    <div >
-                        Usuario parker
-                    </div>
-                    <div>   </div>
-                    <div>   </div>
-                    <div>   </div>
-                    <div>   </div>
-                    <div>   </div>
-                    <div>   </div>
+                < div>
+                    <button className="boton_personalizado">CONTACT</button>
+                    <button className="boton_personalizado">CANCEL</button>
                 </div>
-                <div style={divStyle}>
-                    <div>   </div>
-                    <Button
-                        type="submit"
-                        variant="raised"
-                        color="white"
-                        className="submit"
-
-                    >
-                        contact
-                            </Button>
-                    <div>   </div>
-                    <Button
-                        type="submit"
-                        variant="raised"
-                        color="white"
-                        className="cancel"
-                    >
-                        cancel
-                            </Button>
-                    <div>   </div>
-                    <div>   </div>
-                    <div>   </div>
+                <Divider />
+                <div className="divC"> </div>
+                <Divider />
+                <div className="divC">
+                    <h6>precio</h6>
                 </div>
-
+                <div className="divC"> </div>
+                <Divider />
+                <button className="boton_qr" onClick={this.handleqrButton}>GET YOUR QR CODE</button>
             </div>
 
         );
