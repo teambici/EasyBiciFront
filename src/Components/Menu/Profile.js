4d8dd1e28dfd7ca721ed8b59e792e65379e93ca9
Alejandrito <alejope98@hotmail.com>
2019-10-22 20:39:39 -0500

    navBarPro

--- src/Components/Menu/Profile.js
+++ src/Components/Menu/Profile.js
@@ -4,6 +4,7 @@ import BottomMenu from "../Bottom_Menu"
 import parker from '../../img/Parker.jpg';
 import { Link } from "react-router-dom";
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import NavBar from './NavBarProfile'
 
 export class Profile extends Component {
     constructor(props) {
@@ -21,6 +22,7 @@ export class Profile extends Component {
         return (
             <article>
                 <div style={divStyle}>
+                    <NavBar />
                     <img width='15%' src={parker} alt="mparker" />
                     <div>
                         <h1>Profile</h1>
